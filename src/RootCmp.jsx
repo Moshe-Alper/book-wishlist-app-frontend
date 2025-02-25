fdfd46a9bf4420579aff431c030500b6442e0bed
Moshik Alper <moshikalper@gmail.com>
2025-02-25 16:27:42 +0200

    Add book data and remove socket service

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -5,13 +5,11 @@ import { BookIndex } from './pages/BookIndex.jsx'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
-import { UserMsg } from './cmps/UserMsg.jsx'
 
 export function RootCmp() {
     return (
         <div className="main-container">
             <AppHeader />
-            <UserMsg />
 
             <main>
                 <Routes>
